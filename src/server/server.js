d1c5717c7e321d02b6b2aa9a6be12ce789af273c
Alex Wong <alex.wong@clientrelationship.com>
2019-08-24 10:41:10 +0100

    created a simple server

---
+++ src/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+export default class Server {
+  constructor() {
+    this.app = express()
+    this.app.use(express.static('dist/public'))
+  }
+  start() {
+    return new Promise((resolve, reject) => {
+      try {
+        const PORT = process.env.PORT || 3000
+        this.server = this.app.listen(PORT, function() {
+          resolve(`Server has started on port ${PORT}`)
+        })
+      } catch (error) {
+        console.error(error)
+        reject(error)
+      }
+    })
+  }
+  stop() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.server.close(() => {
+          resolve()
+        })
+      } catch (error) {
+        console.error(error.message)
+        reject(error)
+      }
+    })
+  }
+}
