c6a82cb0ce62185377dfe5585ea1bf20fc94abd0
Alex Wong <alex.wong@clientrelationship.com>
2019-09-07 23:46:22 +0100

    added react

---
+++ src/client/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to my react app</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
